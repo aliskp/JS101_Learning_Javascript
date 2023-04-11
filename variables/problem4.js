6c439c97b9c283a0295c0cf4b116e8ff936529fe
aliskp <alimech369@gmail.com>
2023-04-11 16:33:07 +0530

    variables_Problems_added

---
+++ variables/problem4.js
@@ -0,0 +1,28 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name = "𝓜𝓭 𝓐𝓵𝓲";
+let school = "Ranglal High School";
+let section = "B";
+let grade="A"
+let roll_number = 100064;
+let English = 60;
+let Math = 65;
+let science = 68;
+
+
+console.log("*******************************************");
+console.log("--------  Rᴇᴘᴏʀᴛ Cᴀʀᴅ ᴏғ Sᴛᴜᴅᴇɴᴛ  --------");
+console.log("                                            ")
+console.log("  Name:", name, "|", "Section:", section, "|", "Roll No:", roll_number)
+console.log("                SUBJECTS                            ")
+console.log("                                            ")
+console.log("  English:", English, "Math:",Math, "Science:", science)
+console.log("                                            ")
+console.log("                                            ")
+console.log("  GRADE:", grade)
+
+
+console.log("*******************************************");
+
+
